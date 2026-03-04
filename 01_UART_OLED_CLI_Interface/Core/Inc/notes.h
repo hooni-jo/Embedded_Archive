4f499e8e2438108f49fcd50606ab7bc102427b78
hooni-jo <rudgns4222@naver.com>
2026-03-04 14:05:28 +0900

    refactor: restructure

---
+++ 01_UART_OLED_CLI_Interface/Core/Inc/notes.h
@@ -0,0 +1,21 @@
+/*
+ * notes.h
+ *
+ *  Created on: 2026. 3. 3.
+ *      Author: lenovo
+ */
+
+// notes.h
+#ifndef __NOTES_H__
+#define __NOTES_H__
+
+#define NOTE_C4  262
+#define NOTE_D4  294
+#define NOTE_E4  330
+#define NOTE_F4  349
+#define NOTE_G4  392
+#define NOTE_A4  440
+#define NOTE_B4  494
+#define NOTE_C5  523
+
+#endif
---
+++ 01_UART_OLED_CLI_Interface/Debug/Core/Src/notes.d
@@ -0,0 +1,1 @@
+Core/Src/notes.o: ../Core/Src/notes.c
